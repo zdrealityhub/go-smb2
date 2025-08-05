4aa5022c4115bff2fd3d4e493a401bdeda54dcbb
Mehmet Baker <mehmet@mehmetbaker.dev>
2025-08-05 13:16:11 +0300

    Renamed the module
    Signed-off-by: Mehmet Baker <mehmet@mehmetbaker.dev>

--- internal/ntlm/session.go
+++ internal/ntlm/session.go
@@ -5,7 +5,7 @@ import (
 	"crypto/rc4"
 	"errors"
 
-	"github.com/hirochachacha/go-smb2/internal/utf16le"
+	"github.com/zdrealityhub/go-smb2/internal/utf16le"
 )
 
 type Session struct {
--- session.go
+++ session.go
@@ -12,11 +12,11 @@ import (
 	"fmt"
 	"hash"
 
-	"github.com/hirochachacha/go-smb2/internal/crypto/ccm"
-	"github.com/hirochachacha/go-smb2/internal/crypto/cmac"
+	"github.com/zdrealityhub/go-smb2/internal/crypto/ccm"
+	"github.com/zdrealityhub/go-smb2/internal/crypto/cmac"
 
-	. "github.com/hirochachacha/go-smb2/internal/erref"
-	. "github.com/hirochachacha/go-smb2/internal/smb2"
+	. "github.com/zdrealityhub/go-smb2/internal/erref"
+	. "github.com/zdrealityhub/go-smb2/internal/smb2"
 )
 
 func sessionSetup(conn *conn, i Initiator, ctx context.Context) (*session, error) {
